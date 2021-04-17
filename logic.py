c655d9ab27300b27f46ce7b2dc11eff44a00212b
vityha <dpyg.ttpocto@gmail.com>
2021-04-17 17:30:37 +0300

    123

--- logic.py
+++ logic.py
@@ -65,7 +65,7 @@ def data_base():
 
 def data_lectures():
     db.open_connection()
-    teachers = db.get_all_lectures()
+    teachers = db.get_all_teachers()
     db.close_connection()
     lecturers_data = ObjectForJson()
     lecturers_data.teachers = teachers
